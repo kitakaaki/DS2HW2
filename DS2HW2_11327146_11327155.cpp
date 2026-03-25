e8bc33be64a84568d3dc49de093b6f0ad2d47aaa
kitakaaki <kellenhuang168@gmail.com>
2026-03-25 10:51:24 +0800

    3/25 10:51 update

--- DS2HW2_11327146_11327155.cpp
+++ DS2HW2_11327146_11327155.cpp
@@ -3,6 +3,9 @@
 #include <utility>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 struct Data {
   int serialNumber = 0;
@@ -19,11 +22,11 @@ struct Data {
   std::string systemType;
 };
 
-template <class T1, class T2> class TwoThreeTree {
+class TwoThreeTree {
   private:
     struct Node {
-      T1 key;
-      std::vector<T2> values;
+      int key;
+      std::vector<int> values;
       std::vector<Node*> children;
       Node* parent;
       int keyCount;
@@ -35,20 +38,28 @@ template <class T1, class T2> class TwoThreeTree {
 
     int nodeCount;
   public:
-    void insert(T1 key, T2 value) {}
+    void insert(int key, int value) {}
 
     int height() { return treeHeight; }
 
-    int nodeCount() { return nodeCount; }
+    int count() { return nodeCount; }
 
-    std::pair<T1, std::vector<T2>> rootData() { return std::make_pair(root->key, root->values); }
+    void clear() {}
+
+    std::vector<int> rootData() { 
+      if (root)  
+      return root->values;
+      return {}; 
+    }
+
+    TwoThreeTree() : treeHeight(0), nodeCount(0) {}
 };
 
-template <class T1, class T2> class AVLtree {
+class AVLtree {
   private:
     struct Node {
-      T1 key;
-      std::vector<T2> values;
+      std::string key;
+      std::vector<int> values;
       Node* left;
       Node* right;
       int height;
@@ -68,33 +79,169 @@ template <class T1, class T2> class AVLtree {
 
     void rotateRL(Node* node) {}
   public:
-    void insert(T1 key, T2 value) {}
+    void insert(std::string key, int value) {}
 
     int height() { return treeHeight; }
 
-    int nodeCount() { return nodeCount; }
+    int count() { return nodeCount; }
 
-    std::pair<T1, std::vector<T2>> rootData() { return std::make_pair(root->key, root->values); }
+    void clear() {}
+
+    std::vector<int> rootData() { 
+      if (root)  
+      return root->values;
+      return {}; 
+    }
+
+    AVLtree() : treeHeight(0), nodeCount(0) {}
 };
 
-template <class T1, class T2> class IO {
+class IO {
   public:
-    static void readData(const std::string& fileName, std::vector<std::pair<T1, T2>>& dataList) {}
+    static bool readData(const std::string& fileName, std::vector<Data>& dataList) {
+      std::ifstream file("input" + fileName + ".txt");
+      if (!file.is_open()) {
+        return false;
+      }
+      dataList.clear();
+      for (int i = 0; i < 3; i++) {
+        std::string header;
+        std::getline(file, header);
+      }
+      int serialNumber = 1;
+      std::string line;
+      while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        Data data;
+        data.serialNumber = serialNumber++;
+        std::getline(iss, data.schoolCode, '\t');
+        std::getline(iss, data.schoolName, '\t');
+        std::getline(iss, data.departmentCode, '\t');
+        std::getline(iss, data.departmentName, '\t');
+        std::getline(iss, data.dayNight, '\t');
+        std::getline(iss, data.level, '\t');
+        std::string studentCountStr;
+        std::getline(iss, studentCountStr, '\t');
+        // remove "" and , from the string, e.g. "1,234" -> 1234
+        studentCountStr.erase(std::remove(studentCountStr.begin(), studentCountStr.end(), '"'), studentCountStr.end());
+        studentCountStr.erase(std::remove(studentCountStr.begin(), studentCountStr.end(), ','), studentCountStr.end());
+        data.studentCount = std::stoi(studentCountStr);
+        std::string teacherCountStr, graduateCountStr;
+        std::getline(iss, teacherCountStr, '\t');
+        std::getline(iss, graduateCountStr, '\t');
+        data.teacherCount = std::stoi(teacherCountStr);
+        data.graduateCount = std::stoi(graduateCountStr);
+        std::getline(iss, data.countyCity, '\t');
+        std::getline(iss, data.systemType, '\t');
+        dataList.push_back(data);
+      }
+      file.close();
+      return true;
+    }
 
-    static void displayNode(const std::vector<T2>& values) {}
+    static void displayNodes(const std::vector<Data>& datas) {
+      int serialIndex = 1;
+      for (const auto& data : datas) {
+        std::cout << serialIndex++ << ": [";
+        std::cout << data.serialNumber << "] ";
+        std::cout << data.schoolName << " " << data.departmentName << " ";
+        std::cout << data.dayNight << " " << data.level << " ";
+        std::cout << data.studentCount << " " << data.graduateCount << "\n";
+      }
+    }
 };
 
-template <class T1, class T2> class Menu {
+class Menu {
   private:
-    static void mission1() {}
+    std::vector<Data> dataList;
+    TwoThreeTree twoThreeTree;
+    bool avlTreeExist;
+    AVLtree avlTree;
+    bool dataListExist;
 
-    static void mission2() {}
+    void mission1() {
+      while (true) {
+        std::cout << "Input a file number ([0] Quit): ";
+        std::string fileNumber;
+        std::cin >> fileNumber;
+        if (fileNumber == "0")
+          return;
+        if (!IO::readData(fileNumber, dataList)) {
+          std::cout << "\n### input" << fileNumber << ".txt does not exist! ###\n\n";
+        } else {
+          break;
+        }
+      }
+      avlTreeExist = false;
+      twoThreeTree.clear();
+      dataListExist = true;
+      for (auto& data : dataList) {
+        twoThreeTree.insert(data.graduateCount, data.serialNumber);
+      }
+      std::cout << "Tree height = " << twoThreeTree.height() << "\n";
+      std::cout << "Number of nodes = " << twoThreeTree.count() << "\n";
+      IO::displayNodes(makeNodesList(twoThreeTree.rootData()));
+      std::cout << "\n\n";
+    }
+
+    void mission2() {
+      if (!dataListExist) {
+        std::cout << "### Choose 1 first. ###\n\n";
+        return;
+      }
+      if (!avlTreeExist) {
+        avlTree.clear();
+        for (const auto& data : dataList) {
+          avlTree.insert(data.schoolName, data.serialNumber);
+        }
+      avlTreeExist = true;
+      }
+      if (avlTreeExist)
+        std::cout << "### AVL tree has been built. ###\n";
+      std::cout << "Tree height = " << avlTree.height() << "\n";
+      std::cout << "Number of nodes = " << avlTree.count() << "\n";
+      IO::displayNodes(makeNodesList(avlTree.rootData()));
+      std::cout << "\n\n";
+    }
+
+    std::vector<Data> makeNodesList(const std::vector<int>& pairs) {
+      std::vector<Data> nodesList;
+      for (const auto& pair: pairs) {
+        if (pair - 1 >= 0 && pair - 1 < (int)dataList.size())
+        nodesList.push_back(dataList[pair-1]);
+      }
+      return nodesList;
+    }
   public:
-    static void run() {}
+    void run() {
+      std::string choice;
+      while (true) {
+        std::cout << "* Data Structures and Algorithms *\n";
+        std::cout << "****** Balanced Search Tree ******\n";
+        std::cout << "* 0. QUIT                        *\n";
+        std::cout << "* 1. Build 23 tree               *\n";
+        std::cout << "* 2. Build AVL tree              *\n";
+        std::cout << "**********************************\n";
+        std::cout << "Input a choice(0, 1, 2): ";
+        std::cin >> choice;
+        if (choice == "0") {
+          return;
+        } else if (choice == "1") {
+          mission1();
+        } else if (choice == "2") {
+          mission2();
+        } else {
+          std::cout << "\nCommand does not exist!\n\n";
+        }
+      }
+    }
+
+    Menu() : avlTreeExist(false), dataListExist(false) {}
 };
 
 int main () {
-  Menu<int, Data>::run();
+  Menu menu;
+  menu.run();
   return 0;
 }
 
